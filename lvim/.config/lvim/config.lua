a7bbe2b24e92832d3c9cd4d3141d5b315c38294a
wuzhiqiang <1452366827@qq.com>
2023-10-07 12:11:49 +0800

    wip: 修改配置

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -132,6 +132,35 @@ lvim.plugins = {
 		"folke/trouble.nvim",
 		cmd = "TroubleToggle",
 	},
+  {
+    "nvim-treesitter",
+		event = "BufRead",
+    config = function () 
+    require('nvim-treesitter.configs').setup({
+        -- 支持的语言
+        ensure_installed = {"html", "css", "vim", "lua", "javascript", "typescript", "c", "cpp", "python"},
+        -- 启用代码高亮
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false
+        },
+        --启用增量选择
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = '<CR>',
+                node_incremental = '<CR>',
+                node_decremental = '<BS>',
+                scope_incremental = '<TAB>'
+            }
+        },
+        -- 启用基于 Treesitter 的代码格式化(=)
+        indent = {
+            enable = true
+        },
+    })
+    end,
+  }
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
