1ffe12ddadaed167f22eaa5e0d367185e67db102
wuzhiqiang <1452366827@qq.com>
2023-10-08 09:14:46 +0800

    wip: lvim添加在insert模式下粘贴文本

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -49,6 +49,9 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
+-- insert_mode keymapping
+lvim.keys.insert_mode["<C-v>"] = "<ESC>pa"
+
 -- -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
