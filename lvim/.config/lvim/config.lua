2641f79ebbcbbd40d4a868914fdd9c100c1bd2aa
wuzhiqiang <1452366827@qq.com>
2023-10-08 09:20:55 +0800

    wip: ctrl+b是我设置打开目录的快捷键，不做平滑滚动用

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -162,7 +162,7 @@ lvim.plugins = {
     config = function()
       require('neoscroll').setup({
         -- All these keys will be mapped to their corresponding default scrolling animation
-        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+        mappings = { '<C-u>', '<C-d>', '<C-f>',
           '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
         hide_cursor = true,          -- Hide cursor while scrolling
         stop_eof = true,             -- Stop at <EOF> when scrolling downwards
